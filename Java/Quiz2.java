3ed23add1c54522852938f9d60f110f3d19ff396
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2020-10-29 22:14:51 +0900

    wrote code while tutoring activities

--- Java/Quiz2.java
+++ Java/Quiz2.java
@@ -1,18 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-// 369게임에서 박수치는 경우를 출력하는 프로그램
 public class Quiz2 {
     public static void main(String[] args) {
-        for (int i=1; i<100; i++){
-            if (i%10 == 3|| i%10 == 6||i%10 == 9){
-                if (i/10 == 3|| i/10 == 6||i/10 == 9){
-                    System.out.println(i+" 박수 짝짝");
-                }
-                else
-                    System.out.println(i+" 박수 짝");
-            }
-            else if ((i/10) == 3|| (i/10) == 6 || (i/10)==9)
-                System.out.println(i+" 박수 짝");
-
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("정수:" );
+        for (int i=0; i<6; i++){
+            arr.add(scanner.nextInt());
         }
+
+        int max = 0;
+        for (int i=0; i<arr.size(); i++){
+            if (arr.get(i)>max) max = arr.get(i);
+        }
+        System.out.println("가장 큰 수는 "+max);
     }
 }
